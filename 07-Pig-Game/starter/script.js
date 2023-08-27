ac539407117a3cacec8a009f9f491e78773bb54d
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-27 21:16:56 +0200

    set the scores to 0 and add class

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//selecting the elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice');
+
+//starting condition
+score0El.textContent =0;
+score1El.textContent= 0;
+
+diceEl.classList.add('hidden')
+
+
