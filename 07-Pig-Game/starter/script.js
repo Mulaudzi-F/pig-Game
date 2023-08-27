b8e82af11a230274681c1c55279fd957304ebf9f
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-27 23:19:35 +0200

    add all functionality except newgame btn

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,14 +1,76 @@
 'use strict';
 
 //selecting the elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting condition
 score0El.textContent =0;
 score1El.textContent= 0;
+diceEl.classList.add('hidden');
 
-diceEl.classList.add('hidden')
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
 
+const switchPlayer = ()=>{
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+//rolling dice functionalities
 
+btnRoll.addEventListener('click', function(){
+    //1. generate a random dice roll
+    if(playing){
+    const dice = Math.trunc(Math.random() * 6) +1;
+
+    //2. dice display
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //3. check for rolled1: if true, switch to next player
+
+    if(dice !==1){
+        //add dice to current score
+        currentScore += dice; 
+       document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else{
+        
+        //switch to next player
+       switchPlayer()
+    }
+}
+})
+
+btnHold.addEventListener('click', function(){
+    //1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent= scores[activePlayer] 
+
+    //2. check if player's score is >= 100
+    if(scores[activePlayer] >= 100) {
+        playing = false
+        diceEl.classList.add('hidden');
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner'); 
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+    }else {
+
+        switchPlayer()
+    }
+    //switch to the next player
+   
+})
