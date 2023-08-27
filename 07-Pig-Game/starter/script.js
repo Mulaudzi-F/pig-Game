c4810635648658e64f65034f9dc20e04b2ad3ef0
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-08-27 23:54:47 +0200

    completed the game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,6 +18,7 @@ score0El.textContent =0;
 score1El.textContent= 0;
 diceEl.classList.add('hidden');
 
+
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
@@ -30,9 +31,32 @@ const switchPlayer = ()=>{
     player0El.classList.toggle('player--active');
     player1El.classList.toggle('player--active');
 }
+
+
+
+const init = () =>{
+     scores = [0, 0];
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
+current0El.textContent = 0;
+current1El.textContent =0;
+
+ score0El.textContent =0;
+ score1El.textContent =0;
+ diceEl.classList.add('hidden');
+ 
+ player0El.classList.remove('player--winner')
+ player1El.classList.remove('player--winner')
+
+ player0El.classList.add('plsyer--active')
+ player1El.classList.remove('player--active')
+}
+
 //rolling dice functionalities
 
 btnRoll.addEventListener('click', function(){
+   
     //1. generate a random dice roll
     if(playing){
     const dice = Math.trunc(Math.random() * 6) +1;
@@ -68,9 +92,11 @@ btnHold.addEventListener('click', function(){
     document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
 
     }else {
-
+        
         switchPlayer()
     }
-    //switch to the next player
+   
    
 })
+
+btnNew.addEventListener('click', init)
